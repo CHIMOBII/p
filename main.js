ee98c604f26773b7002b5126f774aa024d4f6bae
CHIMOBII <ezembachima092@gmail.com>
2024-06-07 15:20:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const myVariable = 'chima'
+const myNumber = 45
+
+console.log (myVariable.length)
+console.log (typeof myVariable)
+console.log (typeof myNumber)
+console.log (myVariable.slice(2))
+console.log (myVariable.slice(1,3))
+console.log (myVariable.includes('im'))
